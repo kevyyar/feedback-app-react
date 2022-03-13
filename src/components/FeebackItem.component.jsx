0c6e1751c7abba1428b9cbf210d925ebe2203bb7
Kevin Barreto <barretokevin@hotmail.com>
2022-03-13 11:04:51 -0600

    create Card UI component and update components to have prop types

--- src/components/FeebackItem.component.jsx
+++ src/components/FeebackItem.component.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-
+import PropTypes from 'prop-types';
+import Card from './UI/Card.component';
 
 const FeedbackItem = ({ text, rating }) => {
     return (
-        <div className="card">
+        <Card className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">
                 {text}
             </div>
-        </div>
+        </Card>
     );
+}
+
+FeedbackItem.propTypes = {
+    text: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
 }
 
 export default FeedbackItem;
--- src/components/FeedbackList.component.jsx
+++ src/components/FeedbackList.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import FeedbackItem from './FeebackItem.component';
 
 const FeedbackList = ({ feedback }) => {
     return (
         <>
             {feedback.map(feedback => (
-                <FeedbackItem {...feedback} />
+                <FeedbackItem key={feedback.id} {...feedback} />
             ))}
         </>
     );
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired
+        })
+    )
 }
 
 export default FeedbackList;
