c65ff067ee85b70936fb9a94ae8228dd03e55c0b
Kevin Barreto <barretokevin@hotmail.com>
2022-03-14 09:05:46 -0600

    add context to app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import FeedbackForm from './components/FeedbackForm.component';
 import About from './pages/About.component';
 import AboutIconLink from './components/AboutIconLink.component';
 
+import { FeedbackProvider } from './context/FeedbackContext';
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
@@ -25,23 +27,25 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route exact path="/" element={
-            <>
-              <FeedbackForm onAddFeedback={onAddFeedback} />
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList feedback={feedback} onDeleteFeedback={onDeleteFeedback} />
-            </>
-          }>
-          </Route>
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </BrowserRouter>
+    <FeedbackProvider>
+      <BrowserRouter>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={
+              <>
+                <FeedbackForm onAddFeedback={onAddFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} onDeleteFeedback={onDeleteFeedback} />
+              </>
+            }>
+            </Route>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </BrowserRouter>
+    </FeedbackProvider>
   );
 }
 
